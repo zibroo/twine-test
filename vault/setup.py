1b9a2d55ad9e6b3b1a4953c6ea119f24c3ea41d3
ibraim rakh <ibraim.rakhimdzhanov@gmail.com>
2024-10-21 22:13:12 +0600

    new

--- vault/setup.py
+++ vault/setup.py
@@ -4,12 +4,16 @@ from setuptools import setup
  
 setup(
     name='vault',
-    version='6.0',
+    version='7.0',
     description='Vault authentication and secret retrieval utility',
     author='JFrog',
     author_email='jfrog@jfrog.com',
     license='MIT',
     packages=['vault'],
+    install_requires=[
+            'markdown==3.7',
+            'requests==2.32.3'
+      ],
     python_requires='>=3.8',
     zip_safe=False
 )

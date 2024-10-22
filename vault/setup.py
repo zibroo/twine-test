e43d02c127944e07105afaae306cd09d5d5f549f
ibraim rakh <ibraim.rakhimdzhanov@gmail.com>
2024-10-22 22:24:30 +0600

    test

--- vault/setup.py
+++ vault/setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
  
 from setuptools import setup
- 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"./requirements.txt"
+install_requires = ['markdown==3.7','requests==2.32.3','hvac==2.3.0'] 
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 setup(
     name='vault',
     version='7.0',

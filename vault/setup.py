b18eafcd14698aa149f2c24bc5a566b7dac1aa7c
ibraim rakh <ibraim.rakhimdzhanov@gmail.com>
2024-10-22 22:48:52 +0600

    test

--- vault/setup.py
+++ vault/setup.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
  
 from setuptools import setup
-import os
-lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = f"./requirements.txt"
-install_requires = ['markdown==3.7','requests==2.32.3','hvac==2.3.0'] 
-if os.path.isfile(requirement_path):
-    with open(requirement_path) as f:
-        install_requires = f.read().splitlines()
+
 setup(
     name='vault',
     version='7.1',
@@ -16,7 +10,11 @@ setup(
     author_email='jfrog@jfrog.com',
     license='MIT',
     packages=['vault'],
-    install_requires=install_requires,
+    install_requires=[
+            'markdown==3.7',
+            'requests==2.32.3',
+            'hvac==2.3.0'
+      ],
     python_requires='>=3.8',
     zip_safe=False
 )
